0b734f783cc8783a256a005a0ffc1663f55d847a
Edward Shen <code@eddie.sh>
2022-11-26 13:35:14 -0800

    Fix LRU mention in crate root docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 #![warn(clippy::pedantic, clippy::nursery, clippy::cargo)]
 #![deny(missing_docs)]
 
-//! This crate provides an LRU cache with constant time insertion, fetching,
+//! This crate provides an LFU cache with constant time insertion, fetching,
 //! and removing.
 //!
 //! Storage of values to this collection allocates to the heap. Clients with a
 //! limited heap size should avoid allocating large values and instead use some
 //! form of indirection to avoid a heap overflow.
 //!
-//! Performance of this LRU cache is bounded by constant time operations of a
+//! Performance of this LFU cache is bounded by constant time operations of a
 //! typical unsafe linked list on your platform. For most users, this is an
 //! implementation detail and can be ignored. However, for high throughput
 //! clients, this should be noted as performance might not be as this collection
